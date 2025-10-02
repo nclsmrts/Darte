438c8dd5d643665e6f3aaf53b926ff3a13385aff
nclsmrts <nicolasisabela1412@gmail.com>
2025-10-02 14:49:08 -0300

    feat: subindo

---
+++ Assets/Scripts/Dardos.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Dardos : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private bool hasCollided = false;
+
+    private void Start()
+    {
+        rb.GetComponent<Rigidbody>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (hasCollided) return; // evita m�ltiplas colis�es
+
+        if (collision.gameObject.CompareTag("Alvo"))
+        {
+            hasCollided = true;
+
+            // Para a f�sica do dardo
+            rb.isKinematic = true;
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+
+            // Faz o dardo seguir o alvo
+            //transform.SetParent(collision.transform);
+
+            Debug.Log("bateu");
+        }
+    }
+}
