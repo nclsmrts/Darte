09b796ecc440175fc5f13ca81f88d1864500974a
nclsmrts <nicolasisabela1412@gmail.com>
2025-10-02 17:18:44 -0300

    Feat: arrumando a colisão com o alvo

--- Assets/Scripts/Dardos.cs
+++ Assets/Scripts/Dardos.cs
@@ -1,32 +1,69 @@
 ﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
+[RequireComponent(typeof(Rigidbody), typeof(XRGrabInteractable))]
 public class Dardos : MonoBehaviour
 {
-
     private Rigidbody rb;
     private bool hasCollided = false;
+    private XRGrabInteractable grab;
+
+    public float throwForce = 15f;   // força ao soltar
+    public float stickDepth = 0.05f; // quão fundo o dardo entra no alvo
 
     private void Start()
     {
-        rb.GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        grab = GetComponent<XRGrabInteractable>();
+
+        //comentar isso para funicionar no VR
+        // conectar eventos do XR
+        grab.selectEntered.AddListener(OnGrab);
+        grab.selectExited.AddListener(OnRelease);
     }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (hasCollided) return; // evita m�ltiplas colis�es
+        if (hasCollided) return; // evita múltiplas colisões
 
         if (collision.gameObject.CompareTag("Alvo"))
         {
             hasCollided = true;
 
-            // Para a f�sica do dardo
+            // Para a física do dardo
             rb.isKinematic = true;
             rb.linearVelocity = Vector3.zero;
             rb.angularVelocity = Vector3.zero;
 
-            // Faz o dardo seguir o alvo
-            //transform.SetParent(collision.transform);
+            // Alinha o dardo na superfície
+            ContactPoint contact = collision.contacts[0];
+            transform.position = contact.point - transform.forward * stickDepth;
+            transform.rotation = Quaternion.LookRotation(-contact.normal, Vector3.up);
 
-            Debug.Log("bateu");
+            // Faz o dardo "colar" no alvo
+            transform.SetParent(collision.transform);
+
+            Debug.Log("Dardo grudou no alvo!");
         }
+    }
+
+    void OnGrab(SelectEnterEventArgs args)
+    {
+        hasCollided = false; // reseta quando pegar
+        rb.isKinematic = true; // trava na mão
+        transform.SetParent(args.interactorObject.transform); // cola na mão
+    }
+
+    void OnRelease(SelectExitEventArgs args)
+    {
+        transform.SetParent(null); // solta da mão
+        rb.isKinematic = false;
+
+        rb.linearVelocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        // aplica força pra frente ao soltar
+        rb.AddForce(transform.forward * throwForce, ForceMode.Impulse);
     }
 }
---
+++ Assets/Scripts/SpawnerDardos.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SpawnerDardos : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
